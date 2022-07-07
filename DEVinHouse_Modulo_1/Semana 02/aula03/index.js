ecc713e26e15477312a079f17a59ab27b57c37db
Matheus Vicente <matheusvicentesj5@hotmail.com>
2022-07-06 21:58:42 -0300

    Segunda parte da aula 03

---
+++ DEVinHouse_Modulo_1/Semana 02/aula03/index.js
@@ -0,0 +1,7 @@
+var nome = "Matheus"
+var nome1 = "Matheus"
+var nome2 = "Catarina"
+var data = "02/02/1998"
+var hora = "12:00"
+
+console.log("Caro " + nome +  " Você está convidado(a) para o casamento de " + nome1 + " e " + nome2 + ", a ser realizado no dia " + data + " às " + hora +" horas. ")
