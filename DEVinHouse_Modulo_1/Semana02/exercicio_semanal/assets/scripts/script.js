44e018efa1490b40675f36fc41ac8f467ead9239
Matheus Vicente <matheusvicentesj5@hotmail.com>
2022-07-08 19:41:37 -0300

    [M1S02] Ex 7 - Adicionar funcionalidade ao botão de enviar do formulário

---
+++ DEVinHouse_Modulo_1/Semana02/exercicio_semanal/assets/scripts/script.js
@@ -0,0 +1,8 @@
+function captura(){
+    var nome = document.getElementById("nome").value;
+    var email = document.getElementById("email").value;
+    var sugestao = document.getElementById("sugestao").value;
+    alert(nome)
+    alert(email)
+    alert(sugestao)
+}
