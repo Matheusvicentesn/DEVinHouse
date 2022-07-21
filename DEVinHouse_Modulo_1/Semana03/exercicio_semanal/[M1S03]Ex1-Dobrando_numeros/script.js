7cdc0b3403f61fabf60b519129caa20588c47c67
Matheus Vicente <matheusvicentesj5@hotmail.com>
2022-07-21 20:08:22 -0300

    enviando projeto para git pagaes

--- DEVinHouse_Modulo_1/Semana03/exercicio_semanal/[M1S03]Ex1-Dobrando_numeros/script.js
+++
@@ -1,8 +0,0 @@
-// [M1S03] Ex 1 - Dobrando números
-var listaNumeros = [ 2, 3, 9, 12, 16, 55, 90, 78, 17 ]
-listaNumeros.forEach ( function(valor,indice) {
-    listaNumeros[indice] = (valor * 2);
-    });
-
-console.log(listaNumeros)
-
