add0f4b2bfab2a29435692e4ac17733d292262d0
Matheus Vicente <matheusvicentesj5@hotmail.com>
2022-07-22 10:38:32 -0300

    [M1S04] Ex 7 - Conta bancaria Parte 7

--- DEVinHouse_Modulo_1/Semana04/exercicio_semanal/assets/script/script.js
+++ DEVinHouse_Modulo_1/Semana04/exercicio_semanal/assets/script/script.js
@@ -3,16 +3,19 @@ const contasClientes = [
     id: 0,
     nome: "Matheus Vicente",
     saldo: 1000,
+    senha: "1111",
   },
   {
     id: 1,
     nome: "Maria Luzia",
     saldo: 1555,
+    senha: "2222",
   },
   {
     id: 2,
     nome: "Nicolas Raupp",
     saldo: 1700,
+    senha: "3333",
   },
 ];
 
@@ -66,20 +69,31 @@ function comparar(opcao, valor, contaCliente) {
     sacar(valor, contaCliente);
   } else if (opcao === "depositar") {
     console.log("Depositar selecionado");
-    depositar(valor,contaCliente );
+    depositar(valor, contaCliente);
+  }
+}
+// validacao
+function senha(password, contaCliente, opcao, valor) {
+  let senha2 = document.getElementById("senha");
+  if (contaCliente.senha === password){
+    comparar(opcao, valor, contaCliente);
+  }
+  else{
+    senha2.innerHTML = `Senha incorreta`;
   }
 }
 
 function captura() {
   let id = parseInt(document.getElementById("contas").value);
+  let password = document.getElementById("password").value;
   let valor = parseInt(document.getElementById("valor").value);
   let opcao = document.getElementById("opcao").value;
   let operacao = document.getElementById("operacao");
   let contaCliente = contasClientes.find(function (contaCliente) {
     return contaCliente.id === id;
   });
-  comparar(opcao, valor, contaCliente);
+  senha(password, contaCliente, opcao, valor)
+  // comparar(opcao, valor, contaCliente);
   //teste
   console.log(id, valor, opcao, operacao, contaCliente);
-
 }
