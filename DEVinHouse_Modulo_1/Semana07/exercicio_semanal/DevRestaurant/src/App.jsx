319db4751de3bfc7a30f612092b6a3da0271c553
matheusvicentesn <matheusvicentesj5@hotmail.com>
2022-08-16 17:04:50 -0300

    [M1S07] - Ex 3 - Criar componente de card para exibição dos produtos

--- DEVinHouse_Modulo_1/Semana07/exercicio_semanal/DevRestaurant/src/App.jsx
+++ DEVinHouse_Modulo_1/Semana07/exercicio_semanal/DevRestaurant/src/App.jsx
@@ -1,11 +1,33 @@
 import "./App.css";
+
+// Componentes
+import Cards from "./components/Cards/Cards";
 import Footer from "./components/Footer/Footer";
 import NavBar from "./components/NavBar/NavBar";
+
+// Imagens
+import Coffe from "./assets/img/coffe.jpg";
 
 function App() {
   return (
     <div className="container">
       <NavBar />
+      <div className="gallery">
+        <Cards
+          img={Coffe}
+          nome="Café"
+          descricao="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam euismod ex sit amet eleifend posuere. Vivamus porta venenatis turpis, sit."
+          tempo="15 minutos"
+          preco="10 Reais"
+        />
+        <Cards
+          img={Coffe}
+          nome="Café"
+          descricao="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam euismod ex sit amet eleifend posuere. Vivamus porta venenatis turpis, sit."
+          tempo="15 minutos"
+          preco="10 Reais"
+        />
+      </div>
       <Footer />
     </div>
   );
