623b5f3e1c3a3ae6e87eec928f548ad3b9900c19
matheusvicentesn <matheusvicentesj5@hotmail.com>
2022-08-22 17:02:35 -0300

    [M1S08] Ex 5 - Modificando ListaCards

--- src/components/CriaCards/CriaCards.jsx
+++ src/components/CriaCards/CriaCards.jsx
@@ -1,15 +1,22 @@
 import React, { Fragment } from "react";
 import Cards from "../Cards/Cards";
-import styles from './CriaCards.module.css';
+import styles from "./CriaCards.module.css";
 
-const CriaCards = ({ produtos }) => (
-  <ul className={styles.lista}>
-    {produtos.map((produto) => (
-      <li className={styles.listaItem} key={produto.id}>
-        <Cards produto={produto} selecionado={false} onSelecionar={()=> console.log(`Clicou no card`)}/>
-      </li>
-    ))}
-  </ul>
-);
+const CriaCards = ({ produtos }) => {
+  const [idSelecionados, setIdSelecionados] = useState([])
+  return (
+    <ul className={styles.lista}>
+      {produtos.map((produto) => (
+        <li className={styles.listaItem} key={produto.id}>
+          <Cards
+            produto={produto}
+            selecionado={false}
+            onSelecionar={() => console.log(`Clicou no card`)}
+          />
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 export default CriaCards;
