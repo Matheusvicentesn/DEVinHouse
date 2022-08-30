70588858bb2d38a10d13741a8e501d36b0e0f702
matheusvicentesn <matheusvicentesj5@hotmail.com>
2022-08-30 15:57:24 -0300

    [M1S09] Ex 7 - Criar novo item de menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,11 @@
-import { logo } from '@images';
+import { logo } from "@images";
+import { Link } from "react-router-dom";
 
-import styles from './Header.module.css';
+import styles from "./Header.module.css";
 export const Header = () => (
   <header className={styles.header}>
     <div className={styles.titleContainer}>
-      <img src={logo} alt='Logo do restaurante' height={50} />
+      <img src={logo} alt="Logo do restaurante" height={50} />
       <h1 className={styles.title}>Green Food</h1>
     </div>
 
@@ -12,9 +13,14 @@ export const Header = () => (
       <ul className={styles.ul}>
         <li>
           {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-          <a className={styles.link} href='#'>
+          <Link to="/produtos-selecionado" className={styles.link}>
+            Produtos Selecionados
+          </Link>
+        </li>
+        <li>
+          <Link to="/produtos" className={styles.link}>
             Menu
-          </a>
+          </Link>
         </li>
       </ul>
     </nav>
