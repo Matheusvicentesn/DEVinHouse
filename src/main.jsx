9c669a57ff9428547f530818a83c5d8f1972512b
Matheusvicentesn <matheusvicentesj5@hotmail.com>
2022-09-05 13:52:54 -0300

    [M1S10] Ex 1 - Setup Inicial

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+
+const App = () => {
+  return "Teste"
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
