da305759397e3cee43b89d8d9556f34825de60b1
Matheusvicentesn <matheusvicentesj5@hotmail.com>
2022-09-05 14:27:54 -0300

    [M1S10] Ex 2 - Trazendo o HTML para JSX

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { App } from "./App";
+import "./assets/css/styles.css";
 
-const App = () => {
-  return "Teste"
-}
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
