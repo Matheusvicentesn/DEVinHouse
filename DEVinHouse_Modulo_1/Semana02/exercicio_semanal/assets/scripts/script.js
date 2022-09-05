729fd6d41ada943d0be6c4941f31f5b81bafa24a
Matheusvicentesn <matheusvicentesj5@hotmail.com>
2022-09-05 13:22:58 -0300

    inicial commit

--- DEVinHouse_Modulo_1/Semana02/exercicio_semanal/assets/scripts/script.js
+++
@@ -1,8 +0,0 @@
-function captura(){
-    var nome = document.getElementById("nome").value;
-    var email = document.getElementById("email").value;
-    var sugestao = document.getElementById("sugestao").value;
-    alert(nome)
-    alert(email)
-    alert(sugestao)
-}
--- DEVinHouse_Modulo_1/Semana05/exercicio_semanal/[M1S05]_Ex_2  /saudacao_especial.js
+++
@@ -1,5 +0,0 @@
-const saudacaoEspecial = (nome = "convidado") => {
-  return `Ola, ${nome}! Seja muito bem vindo`;
-};
-
-export default saudacaoEspecial;
