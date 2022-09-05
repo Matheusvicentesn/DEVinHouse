fbf5cdbfdf81bc3cfbc7bf34294a405487853559
Matheusvicentesn <matheusvicentesj5@hotmail.com>
2022-09-05 16:56:57 -0300

    [M1S10] Ex 7 - Criando o contexto

---
+++ src/context/app-context.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const appContext = React.createContext(null);
+const ContextProvider = appContext.Provider;
+
+export const useAppContext = () => {
+  const context = React.useContext(appContext);
+  return context;
+};
+
+export const AppContextProvider = ({ children }) => {
+  return <ContextProvider value={"conetado!"}>{children}</ContextProvider>;
+};
