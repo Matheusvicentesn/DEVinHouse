c3670553e9ce774e595289b78fa2bf1ea5bc64d4
Matheusvicentesn <matheusvicentesj5@hotmail.com>
2022-09-25 11:35:45 -0300

    [M1S11] Ex 2 - Configuração do tema utilizando styled components

---
+++ src/themes/themeDark.js
@@ -0,0 +1,8 @@
+export const themeDark = {
+  colors: {
+    backgroundPrimary: "#0D1821",
+    backgroundSecondary: "#312f2f",
+    primary: "#FFF",
+    secondary: "#183642",
+  },
+};
---
+++ src/themes/themeLight.js
@@ -0,0 +1,8 @@
+export const themeLight = {
+  colors: {
+    backgroundPrimary: "#dddd",
+    backgroundSecondary: "#bebebe",
+    primary: "#73628a",
+    secondary: "#ffbcb5",
+  },
+};
