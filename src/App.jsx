cce04c14936e86824356111c10f605601043455c
Matheusvicentesn <matheusvicentesj5@hotmail.com>
2022-09-25 12:43:41 -0300

    [M1S11] Ex 4 - Criar o layout da aplicação

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import { CustomThemeProvider } from "./contexts";
+import { AppLayout } from "./layouts";
+import { GlobalStyle } from "./themes";
 function App() {
   return (
     <CustomThemeProvider>
+      <GlobalStyle />
+      <AppLayout />
       <p>Olá</p>
     </CustomThemeProvider>
   );
