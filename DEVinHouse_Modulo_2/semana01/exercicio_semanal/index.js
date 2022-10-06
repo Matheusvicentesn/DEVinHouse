618a8d07ea66322fd4044c0cd3326dfa2013464a
Matheusvicentesn <matheusvicentesj5@hotmail.com>
2022-10-06 17:43:24 -0300

    [M2S02] Ex 2 - Rotas de gerenciamento de pizzas

--- DEVinHouse_Modulo_2/semana01/exercicio_semanal/index.js
+++ DEVinHouse_Modulo_2/semana01/exercicio_semanal/index.js
@@ -1,6 +1,27 @@
 const express = require("express");
-
+const { v4: uuidv4 } = require("uuid");
 const app = express();
+app.use(express.json());
+let pizzas = [];
+
+//Buscar Pizzas
+app.get("/pizzas", (request, response) => {
+  response.json(pizzas);
+});
+
+//Cadastrar Pizzas
+app.post("/pizzas", (request, response) => {
+  const pizza = {
+    id: uuidv4(),
+    name: request.body.name,
+    description: request.body.description,
+    price: request.body.price,
+    ingredients: request.body.ingredients,
+  };
+  pizzas.push(pizza);
+
+  response.status(201).json(pizza);
+});
 
 app.listen(3333, () => {
   console.log("Servidor Rodando");
